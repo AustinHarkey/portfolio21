1311301a35283ad063284fefac29ae3235d89fc4
Austin Harkey <austindharkey@gmail.com>
2021-11-15 16:11:55 -0800

    theme updates

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const icon = document.querySelector('.triple__icon');
 
 
 toggle.addEventListener('click', (e) => {
-    if(stylesheet.getAttribute('href') == 'style-dark.css') {
+    if(stylesheet.getAttribute('href') == 'style-navy.css') {
         stylesheet.setAttribute('href', 'style.css');
         toggle.innerHTML = '<i class="far fa-moon"></i>';
         logo.src = 'img/logo-dark-2.png';
         icon.src = 'img/code2.png';
     } else {
-        stylesheet.setAttribute('href', 'style-dark.css');
+        stylesheet.setAttribute('href', 'style-navy.css');
         toggle.innerHTML = '<i class="far fa-sun"></i>';
         logo.src = 'img/logo-dark-2-green.png';
         icon.src = 'img/code2-green.png';
