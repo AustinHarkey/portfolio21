121ad1002c00f3180f4f54e6e45d3978e9329422
Austin Harkey <austindharkey@gmail.com>
2021-11-20 23:28:41 -0800

    mobile menu

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 const links = document.querySelectorAll('.nav__link');
+const header = document.querySelector('.header');
 let menuOpen = false;
-
-
 
 burger.addEventListener('click', () => {
     if(!menuOpen) {
@@ -15,12 +14,38 @@ burger.addEventListener('click', () => {
         nav.classList.remove('nav--open');
         menuOpen = false;
         
+    }
+})
+
+const linkClose = () => {
         links.forEach(link => {
             link.addEventListener('click', () => {
                 burger.classList.remove('open');
                 nav.classList.remove('nav--open');
                 menuOpen = false;
+                console.log('pops');
             })
         });
+}
+
+linkClose();
+
+
+window.onscroll = () => {
+    var top = window.scrollY;
+    var width = window.innerWidth;
+    if(top >= 100 && width > 1200) {
+        header.classList.add('header--active');
+    } else {
+        header.classList.remove('header--active');
     }
-})
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(ref => {
+    ref.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: 'smooth'
+        });
+    })
+});
